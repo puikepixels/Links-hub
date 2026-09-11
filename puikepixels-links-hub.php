23ebbea9db60c0f6cf79445c199a764d30de9875
puikepixels <bram@puikepixels.com>
2026-09-11 08:23:42 +0200

    Add Dutch and English language support with .po and .mo files, and update plugin metadata

--- puikepixels-links-hub.php
+++ puikepixels-links-hub.php
@@ -8,6 +8,7 @@
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: puikepixels-links-hub
+ * Domain Path: /languages
  */
 
 declare(strict_types=1);
